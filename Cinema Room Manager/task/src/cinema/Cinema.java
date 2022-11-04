b915628550d0b86006330e8d8226e7f03a6195b4
kislyakovm <maksim@druble.ru>
2022-11-04 05:49:17 +0300

    Initial commit

---
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -0,0 +1,19 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("""
+                Cinema:
+                  1 2 3 4 5 6 7 8
+                1 S S S S S S S S
+                2 S S S S S S S S
+                3 S S S S S S S S
+                4 S S S S S S S S
+                5 S S S S S S S S
+                6 S S S S S S S S
+                7 S S S S S S S S""");
+    }
+}
